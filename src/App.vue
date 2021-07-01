53fce0392398d7a44b5d2be48e45a5d32bd9f234
dawit95 <dawit0310@gmail.com>
2021-07-01 17:30:11 +0900

    jenkins test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to 다윗! Vue.js App" />
+  <HelloWorld msg="Welcome to Jenkins ! Vue.js App" />
 </template>
 
 <script>
