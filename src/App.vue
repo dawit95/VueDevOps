8cfbae0385237b3b324c5fa22ecc2d7c686bfb80
dawit95 <dawit0310@gmail.com>
2021-07-01 16:47:57 +0900

    modify text jenkins

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to 최다윗! Vue.js App" />
+  <HelloWorld msg="Welcome to Jenkins! Vue.js App" />
 </template>
 
 <script>
